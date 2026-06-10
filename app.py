d4990cd80bba77d0fadba9e38289a0bc4109a94e
nancysoni796-jpg <nancysoni796@gmail.com>
2026-06-10 23:35:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ answer_style = st.sidebar.selectbox(
 )
 if api_key:
     genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-pro")
+model = genai.GenerativeModel("gemini-pro")
 learning_level = st.selectbox(
     "Select Learning Level",
     ["School", "Intermediate", "College"]
