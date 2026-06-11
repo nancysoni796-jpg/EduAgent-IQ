cb308c07c8c6b126ac0e86d521155ce64ffb0817
nancysoni796-jpg <nancysoni796@gmail.com>
2026-06-11 09:22:36 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -7,3 +7,12 @@ def register(username, password):
 
 def login(username, password):
     return db.login_user(username, password)
+from db import Database
+
+_db = Database()
+
+def register(username, password):
+    return _db.add_user(username, password)
+
+def login(username, password):
+    return _db.login_user(username, password)
